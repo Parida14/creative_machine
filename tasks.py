bf17f45a6f506259664cd71cccc7056c800ca75d
Vinicius Goecks <vinicius.goecks@tamu.edu>
2016-10-29 19:39:11 -0500

    version 1.0 of task generator

---
+++ tasks.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""tasks.py: work on questins for the creative machine
+"""
+
+__author__ = "Vinicius Guimaraes Goecks"
+__copyright__ = "TBD"
+__collaborators__ = ["Mauricio Coen", "Aniket Bonde", "Lagnajit Parida"]
+__license__ = "TBD"
+__version__ = "0.0.0"
+__maintainer__ = "Vinicius Guimaraes Goecks"
+__email__ = "viniciusguigo@gmail.com"
+__status__ = "Prototype"
+__date__ = "October 29, 2016"
+
+# import
+import random
+
+
+class ManageTaks():
+    """
+    Manage everything related to the tasks.
+
+    Methods
+    ---------
+    gen_task = generates new tasks
+    check_repeated_task = i will let you guess that
+    """
+    def ___init___(self):
+        self.tasks_asked = [] # empty list of tasks asked
+
+    def gen_task(self, q_type="write", q_option="food"):
+        """
+        Generate tasks based on inputs.
+
+        Inputs
+        ---------
+        type = write, draw (action to be proposed)
+        option = item (the categorie to be asked about)
+
+        Outputs
+        ---------
+        task = string with task
+        data = output the selected q_type and q_option to create a future list
+               and block repeated tasks.
+        """
+        # check what it the task about
+        if q_type == "write":
+            question = ["list 5 of your favorite ", "write about "]
+            select_question = random.choice(question)
+
+        elif q_type == "draw":
+            draw = ["draw your favorite ",
+                    "draw the first thing it comes to your mind when you see a "]
+            select_question = random.choice(draw)
+
+        # output the task based on the option input
+        select_option = q_option
+
+        # join everything
+        task = select_question + select_option
+
+        return task
+
+    def check_repeated_task(self, task):
+        """
+        Check if this was already asked.
+
+        Inputs
+        ---------
+        task = string containing q_type+q_option
+
+        Outputs
+        ---------
+        task_status = if asked or not (False, True)
+        """
+        task_status = task in self.tasks_asked
+
+        # append if never asked
+        if task_status == False:
+            self.tasks_asked.append(task)
+
+        return task_status
