edbe647247f5757d6e5f5836f4f383ca8707aed5
Vinicius Goecks <vinicius.goecks@tamu.edu>
2016-10-30 01:43:04 -0500

    fixed repeated questions

--- test_run.py
+++ test_run.py
@@ -23,6 +23,7 @@ from manage_input import ManageInputs
 number_of_days = 5
 
 # initialize all
+input_manager = ManageInputs()
 task_manager = ManageTasks()
 task_manager.initialize_class() # init not working?
 
@@ -35,36 +36,66 @@ for day in range(number_of_days):
     print("\n*** Day %i ***" %day)
 
     # get input (ask learning questions)
-    print("***")
-    q_option = random.choice(database.learning_database)
-    input_question = input_data.gen_learning_question(q_option+": ")
-    user_input = input(input_question)
-    print(user_input)
+    check_input = True
+    count_input = 0
+    while check_input == True: # make sure it is not repeated
+        q_option = random.choice(database.learning_database)
+        input_question = input_data.gen_learning_question(q_option)
 
-    # extract nouns and send to new database
-    new_nouns = get_nouns(user_input)
-    for each_entry in new_nouns:
-        database.new_database.append(each_entry)
+        # check repeated
+        check_input = input_manager.check_repeated_inputs(input_question)
+        count_input += 1
+        # try 10 times
+        if count_input >= 3:
+            input_question = "No questions this time!"
+            check_input = False
+            break
+
+    # ask input
+    if input_question == "No questions this time!":
+        print("No questions this time!")
+    else:
+        # get input
+        user_input = input(input_question+": ")
+
+        # extract nouns and send to new database
+        new_nouns = get_nouns(user_input)
+        for each_entry in new_nouns:
+            database.new_database.append(each_entry)
 
     # clean duplicates on new database
     database.new_database = list(set(database.new_database))
 
     # get answer (from task)
     # select between write and draw
-    q_new_option = random.choice(database.new_database)
-    coin = random.random()
-    if coin >= 0.5:
-        task = task_manager.gen_task('write',q_new_option)
+    check_task = True
+    count_task = 0
+    if len(database.new_database): # check if empty
+        while check_task == True: # make sure it is not repeated
+            q_new_option = random.choice(database.new_database)
+            coin = random.random()
+            if coin >= 0.5:
+                task = task_manager.gen_task('write',q_new_option)
+            else:
+                task = task_manager.gen_task('draw',q_new_option)
+
+            # check repeated
+            check_task = task_manager.check_repeated_task(task)
+            count_task += 1
+            # try 10 times
+            if count_task >= 3:
+                task = "No questions this time!"
+                check_task == False
+                break
+
+        # ask the task
+        if task == "No questions this time!":
+            print("No questions this time!")
+        else:
+            task_answer = input(task+": ")
+
     else:
-        task = task_manager.gen_task('draw',q_new_option)
-
-    # check repeated
-    check_task = task_manager.check_repeated_task(task)
-    print("***")
-
-    # ask the task
-    task_answer = input(task+": ")
-    print(task_answer)
+        print("No task this time!")
 
 # add new nouns to database
 
